3357fdfc871cd07354043f05b1c352d57d69d96a
Yordan Velikov <yordan.velikov@gmail.com>
2017-01-21 11:25:49 +0200

    Update Program.cs

--- 01. First Steps in Coding/01. Hello C#/Program.cs
+++ 01. First Steps in Coding/01. Hello C#/Program.cs
@@ -6,7 +6,7 @@ namespace example2_expression
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
+			Console.WriteLine("Hello C#");
 			
 		}
 	}
